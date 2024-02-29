8f58a30277239923a603a3fe6bfd46bdd9bce0c5
evandrocasliniifsp <evandro.caslini@aluno.ifsp.edu.br>
2024-02-29 15:20:21 -0300

    corrigindo ordem

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const vermelho = document.querySelector('.vermelho');const amarelo = document.querySelector('.amarelo');const verde = document.querySelector('.verde');
+const vermelho = document.querySelector('.vermelho');
+const amarelo = document.querySelector('.amarelo');
+const verde = document.querySelector('.verde');
 
-let contador = 1;
+let contador = 0;
 
 setInterval(() => {
-	if(contador <= 1) {
-		vermelho.classList.add('vermelho-ativo');
-	} else if (contador === 2) {
-		amarelo.classList.add('amarelo-ativo');
-		vermelho.classList.remove('vermelho-ativo');
-	} else if (contador === 4) {
-		verde.classList.add('verde-ativo');
-		amarelo.classList.remove('amarelo-ativo');
-	} else if (contador === 8) {
-		verde.classList.remove('verde-ativo');
-		contador = 0;
-	}
-	
-	contador += 1;
+    if (contador === 0 || contador === 6) {
+        vermelho.classList.add('vermelho-ativo');
+        verde.classList.remove('verde-ativo');
+        amarelo.classList.remove('amarelo-ativo');
+    } else if (contador === 3) {
+        verde.classList.add('verde-ativo');
+        vermelho.classList.remove('vermelho-ativo');
+        amarelo.classList.remove('amarelo-ativo');
+    } else if (contador === 4) {
+        amarelo.classList.add('amarelo-ativo');
+        vermelho.classList.remove('vermelho-ativo');
+        verde.classList.remove('verde-ativo');
+    } else if (contador === 7) {
+        amarelo.classList.remove('amarelo-ativo');
+    }
+    
+    contador = (contador + 1) % 8;
 }, 1000);
